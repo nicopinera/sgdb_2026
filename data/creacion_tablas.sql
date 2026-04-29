1bf984e628558af532a48b2eea09b074c0446553
Nicolas Piñera <nicolaspinera@gmail.com>
2026-04-29 09:13:30 -0300

    Creacion: script sql para creacion de tablas

---
+++ data/creacion_tablas.sql
@@ -0,0 +1,35 @@
+-- Creacion Esquema/base de datos
+create schema tp2;
+
+-- Creacion tabla personas
+create table persona();
+
+-- Creacion tabla afiliado
+create table afiliado();
+
+-- Creacion tabla comprador
+create table comprador();
+
+-- Creacion tabla Rifa
+create table rifa();
+
+-- Creacion tabla Sorteo
+create table sorteo();
+
+-- Creacion tabla Grupo
+create table grupo();
+
+-- Creacion tabla Historial_Gripo
+create table historialGrupo();
+
+-- Creacion tabla Historial_Venta
+create table historialVenta();
+
+-- Creacion tabla Coutas
+create table couta();
+
+-- Creacion tabla infoCoutas
+create table infoCuotas();
+
+-- Creacion tabla HistorialCompra
+create table historialCompra();
